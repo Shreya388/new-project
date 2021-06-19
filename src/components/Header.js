24464f2928cd816ead2d17ac8c97b6f3070c35e7
Shreya388 <xjshreya39@gmail.com>
2021-06-19 22:26:56 +0530

    imported Header in App.js and added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
-const Header = ({ title }) => {
+import { Navbar, Nav} from "react-bootstrap"
+
+const Header = () => {
     return (
         <header>
-            <div className='container'>
-                <h1>{title}</h1>
+            <div>
+            <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      
+          
+      </Navbar>
             </div>
         </header>
     )
 }
 
-Header.defaultProps = {
-    title: 'Task Tracker',
-}
+
 
 export default Header;
