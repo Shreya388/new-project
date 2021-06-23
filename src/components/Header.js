b0e06f979c4fcf72690c026823e163414e98dd45
Shreya388 <xjshreya39@gmail.com>
2021-06-23 23:51:23 +0530

    blurred cover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { Navbar, Nav, Container} from "react-bootstrap"
+import { Navbar, Nav, Container, Col} from "react-bootstrap"
 import "./style.css"
 
-var headText = "Yo, What up";
-const brandName = "QUOTO";
+
+var headPara = "Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin at nisl sagittis, ornare ipsum non,"
+var headText = "Yo, What up :)";
+const brandName = "Quoto";
+
 
 const Header = () => {
     return (
@@ -10,8 +13,11 @@ const Header = () => {
         <div>
             <div className="cover">
                 <div className="cover_overlay">
-                    <Container>
-                        <h1 className="display-3 text-white Heading">{headText}</h1>
+                    <Container style={{margin: '150pt'}}>
+                        <Col xs={5}>
+                        <h1 className="display-3 Heading" style={{ color: 'yellow' }}>{headText}</h1>
+                        <p style={{color:'#c7c7c7'}}>{headPara}</p>
+                        </Col>
                     </Container>
                 </div>
             </div>
@@ -19,13 +25,14 @@ const Header = () => {
             {/*Navbar section*/}
             <Navbar className="nav">
                 <Container>
-                <Navbar.Brand style={{fontSize: '23px', color: '#84801A', fontWeight: 'bold', fontFamily: 'Source Sans Pro'}}>{brandName}</Navbar.Brand>
+                <Navbar.Brand style={{fontSize: '23px', color: '#84801A', padding: '15pt', fontWeight: 'bold', fontFamily: 'Source Sans Pro'}}>{brandName}</Navbar.Brand>
                 
                 <Container>
                     <Nav className="mr-auto ">
-                        <a className="link" href="#home">Home</a>
+                        <a className="alink" href="#home">Home</a>
                         <a className="link" href="#features">Features</a>
                         <a className="link" href="#pricing">Pricing</a>
+                        <a className="link" href="#contact">Contact</a>
             
                     </Nav>
                     </Container>
